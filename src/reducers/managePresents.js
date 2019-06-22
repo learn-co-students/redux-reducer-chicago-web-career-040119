31adb9249ca05a3e3c9a8d5e84930c4970d9c807
Sonia Dumitru <soni.dumitru@gmail.com>
2019-06-22 13:55:02 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,8 +1,23 @@
-export function managePresents(state = {numberOfPresents: 0}, action){
+// export function managePresents(state = {numberOfPresents: 0}, action){
+//   switch(action.type) {
+//     case "INCREASE" :
+//       return {...state, numberOfPresents: state.numberOfPresents + 1}
+//     default:
+//       return state;
+//   }
+// }
+
+export function managePresents(state = {
+  numberOfPresents: 0,
+}, action) {
   switch(action.type) {
-    case "INCREASE" :
-      return {...state, numberOfPresents: state.numberOfPresents + 1}
+
+    case "INCREASE":
+      return Object.assign({}, state, {
+        numberOfPresents: state.numberOfPresents + 1
+      });
+
     default:
       return state;
   }
-}
+};
